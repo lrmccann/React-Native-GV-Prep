7a4591b791a305f834fa2206b121223a613e7e72
Logan McCAnn <loganrmccann@gmail.com>
2021-03-16 19:25:42 -0500

    updated pod dependencies for native modules library; RCT header for native calendar, impleementing the bridge protocol;

---
+++ ios/GrapeVineAI/RCTCalendarModule.h
@@ -0,0 +1,16 @@
+//
+//  RCTCalendarModule.h
+//  GrapeVineAI
+//
+//  Created by Logan Mccann on 3/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol RCTCalendarModule <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,10 @@
+//
+//  RCTCalendarModule.h
+//  GrapeVineAI
+//
+//  Created by Logan Mccann on 3/16/21.
+//
+
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
